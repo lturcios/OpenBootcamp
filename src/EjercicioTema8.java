f4801b0b68a3c3885e49a39c7b3d39aec2d94c08
Luis Turcios <l.turcios.08@gmail.com>
2023-04-09 20:06:58 -0600

    Tercer commit: Se agregan ejercicios correspondientes al tema 8

---
+++ src/EjercicioTema8.java
@@ -0,0 +1,42 @@
+public class EjercicioTema8 {
+    public static void main(String[] args) {
+        Persona persona = new Persona();
+        persona.setEdad(43);
+        persona.setNombre("Luis Turcios");
+        persona.setTelefono("50369866023");
+        System.out.println("Nombre: " + persona.getNombre());
+        System.out.println("Edad: " + persona.getEdad());
+        System.out.println("Telefono: " + persona.getTelefono());
+    }
+}
+
+class Persona{
+    private int edad;
+    private String nombre;
+    private String telefono;
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+}
+
